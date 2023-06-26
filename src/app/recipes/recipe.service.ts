215972bf03d400b867bcc11c70984b4836a553c3
SankarshanRamesh <Sankarshanr@gmail.com>
2023-06-26 22:33:21 +0530

    added recipe and shopping list services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,34 @@
-import { Recipe } from "./recipe.model";
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
-    recipes: Recipe[] = [
-        new Recipe(
-          'A Test Recipe',
-          'This is simply a test',
-          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-        ),
-        new Recipe(
-          'Another Test Recipe',
-          'This is simply a test',
-          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-        ),
-      ];
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Dosa',
+      'Very crispy and Tasty!',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('Batter', 1), new Ingredient('Chutney', 1)]
+    ),
+    new Recipe(
+      'Idli',
+      'Very Very soft and Tasty!',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('Rava', 2), new Ingredient('Sambhar', 2)]
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,12 +1,6 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  OnInit,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -17,16 +11,15 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
-  constructor() {}
+  constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {}
 
   onAddClick() {
-    const ingredient =  new Ingredient(
+    const ingredient = new Ingredient(
       this.nameInputRef.nativeElement.value,
       this.amountInputRef.nativeElement.value
     );
-    this.ingredientAdded.emit(ingredient);
+    this.shoppingListService.addIngredient(ingredient);
   }
 }
